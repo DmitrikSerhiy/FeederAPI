14fc5bec00fd3404b4e04de1bc076562cf08126a
Сергей Дмитрик <Dr.voo1646@gmail.com>
2018-09-20 16:47:06 +0300

    added project infrastructure. added IoC container. added repositories. generated first db structure.

---
+++ Feeder.Infrastructure/DependencyResolver.cs
@@ -0,0 +1,19 @@
+﻿using Feeder.DAL;
+using Feeder.DAL.Interfaces;
+using Freeder.BLL;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Feeder.Infrastructure
+{
+    public class DependencyResolver
+    {
+        public DependencyResolver(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<FeedContext>();
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+            serviceCollection.AddScoped<IRssRepository, RssRepository>();
+            serviceCollection.AddScoped<RssService>();
+        }
+    }
+}
---
+++ Freeder.BLL/RssService.cs
@@ -0,0 +1,52 @@
+﻿using Feeder.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Freeder.BLL
+{
+    public class RssService
+    {
+        private IRssRepository rssRepo;
+        public RssService(IUnitOfWork UnitOfWork)
+        {
+            rssRepo = UnitOfWork.Rsses;
+        }
+
+        public RssDTO GetFeed(int Id)
+        {
+            var rss = rssRepo.GetFeed(Id);
+            //map
+            return new RssDTO()
+            {
+                Title = "test feed1",
+                Content = "some content1",
+                Link = "some url1",
+                Description = "some desc1"
+            };
+        }
+
+        public IEnumerable<RssDTO> GetFeeds()
+        {
+            var rss = rssRepo.GetFeeds();
+            //map
+            var rssDTO = new List<RssDTO>();
+            rssDTO.Add(new RssDTO()
+            {
+                Title = "test feed1",
+                Content = "some content1",
+                Link = "some url1",
+                Description = "some desc1"
+            });
+            rssDTO.Add(new RssDTO()
+            {
+                Title = "test feed2",
+                Content = "some content2",
+                Link = "some url2",
+                Description = "some desc2"
+            });
+
+            return rssDTO;
+        }
+
+    }
+}
